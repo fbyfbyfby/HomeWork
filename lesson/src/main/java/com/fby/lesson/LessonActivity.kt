8992e5d447a9d092ccdd8c043df162c110ffe540
bojan <fengboyang@sina.com>
2021-08-25 14:55:00 +0800

    依赖更改

---
+++ lesson/src/main/java/com/fby/lesson/LessonActivity.kt
@@ -0,0 +1,11 @@
+package com.fby.lesson
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LessonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lesson)
+    }
+}
